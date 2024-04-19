8903b44fbf47e6b9c71ce1b135d2fd099944003e
Klajdi Bodurri <kbodurri>
2024-04-19 13:46:57 +0300

    Replace EnableSQL with USE_POSTGRES

--- locallibrary_tutorial/locallibrary/settings.py
+++ locallibrary_tutorial/locallibrary/settings.py
@@ -71,14 +71,7 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-if os.getenv('ENABLE_SQLITE3'):
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
+if os.getenv('USE_POSTGRES'):
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
@@ -87,6 +80,13 @@ else:
             'PASSWORD': '1234',
             'HOST': 'postgres_db',
             'PORT': '5432',
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 
